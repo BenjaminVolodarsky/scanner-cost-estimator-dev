72ed62de95192598e919d30727019713cd60cd5a
Benny Volodarsky <benny.volodarsky@upwind.io>
2026-02-02 15:02:58 +0200

    p

--- collectors/s3.py
+++ collectors/s3.py
@@ -25,7 +25,7 @@ def collect_s3_buckets(session, account_id="unknown"):
             # We look back 2 days to ensure we find a daily metric point
             size_gb = 0
             try:
-                metrics = cw.get_metric_statistics(
+                size = cw.get_metric_statistics(
                     Namespace='AWS/S3',
                     MetricName='BucketSizeBytes',
                     Dimensions=[
@@ -38,19 +38,38 @@ def collect_s3_buckets(session, account_id="unknown"):
                     EndTime=now
                 )
 
-                if metrics['Datapoints']:
+                if size['Datapoints']:
                     # Convert Bytes to GB
-                    bytes_val = metrics['Datapoints'][0]['Average']
+                    bytes_val = size['Datapoints'][0]['Average']
                     size_gb = round(bytes_val / (1024 ** 3), 2)
             except:
                 pass  # Default to 0 if metrics are inaccessible
+
+            doc_num = 0
+            try:
+                doc_num = cw.get_metric_statistics(
+                    Namespace='AWS/S3',
+                    MetricName='NumberOfObjects',
+                    Dimensions=[
+                        {'Name': 'BucketName', 'Value': name},
+                        {'Name': 'StorageType', 'Value': 'AllStorageTypes'}
+                    ],
+                    Statistics=['Average'],
+                    Period=86400,  # 24 hours in seconds
+                    StartTime=now - timedelta(days=2),
+                    EndTime=now
+                )
+
+            except:
+                pass
 
             results.append({
                 "account_id": account_id,
                 "resource": "s3_bucket",
                 "bucket_name": name,
                 "region": loc,
-                "size_gb": size_gb
+                "size_gb": size_gb,
+                "doc_num": doc_num
             })
     except Exception:
         return []
